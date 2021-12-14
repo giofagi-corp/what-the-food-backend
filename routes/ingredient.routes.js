b1aceb21c5a460d4a7d52bfce0fe279635570ffe
clorodino <juanluiscas@gmail.com>
2021-12-14 09:28:55 +0100

    search by ingredients

--- routes/ingredient.routes.js
+++ routes/ingredient.routes.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const Ingredient = require("../models/Ingredient.model");
 const Recipe = require("../models/Recipe.model");
+let ObjectId = require('mongodb').ObjectId;
 
 //CREATE AN INGREDIENT
 
@@ -11,7 +12,7 @@ router.post("/ingredient/create", async (req, res) => {
   try {
     const { img, name, type } = req.body;
     const newIngredient = await Ingredient.create({ img, name, type })
-      .res.status(201)
+      res.status(201)
       .json(newIngredient);
   } catch (err) {
     console.log(err);
@@ -67,5 +68,18 @@ router.get("/top-ingredients", async (req, res) => {
       res.status(200).json(topIngredients);
   } catch (err) {
       console.log(err);
+  }
+});
+
+
+// SEARCH by INGREDIENTS 
+
+router.get("/recipes", async (req, res) => {
+  const arrIngredientsID  = req.query.ingredients.split(" ")
+  try {
+    const filteredRecipes = await Recipe.find({ingredients: { $in: arrIngredientsID}  })
+    res.status(200).json(filteredRecipes);
+  } catch (err) {
+    console.log(err);
   }
 });
