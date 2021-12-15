5dd69c1cc96b87744deebf4048c10cb145d00f16
Jordi Roca <jordirocasoler94@gmail.com>
2021-12-15 09:54:20 +0100

    wip

--- routes/ingredient.routes.js
+++ routes/ingredient.routes.js
@@ -9,7 +9,8 @@ let ObjectId = require("mongodb").ObjectId;
 //SEARCH AN INGREDIENT TO ADD TO A RECIPY ( Create Ingredient if it does not exist)
 
 router.post("/search-ingredient", async (req, res) => {
-  const { ingredient:name } = req.body;
+  console.log("REQ BODY---->",req.body)
+  const {ingredient:name } = req.body;
   try {
     const ingredientFound = await Ingredient.find({ name: name });
     console.log("ing FOUND---->", ingredientFound);
